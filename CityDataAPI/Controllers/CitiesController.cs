81cd7f9a8cd884108788048032ca63ffa0cfdb23
Gabriel Mello <britogabrieldemello@gmail.com>
2023-07-14 01:49:47 -0300

    project configuration. Applied a get to list the cities registered in the api.

---
+++ CityDataAPI/Controllers/CitiesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityDataAPI.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController: ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<object>
+                {
+                    new {id = 1, Name = "São Paulo"},
+                    new {id = 2, Name = "Salvador"}
+                }
+            );
+        }
+    }
+}
