961d1b5758c5176f558c24c850dbfccf1d9f7a7a
Gabriel Mello <britogabrieldemello@gmail.com>
2023-07-22 16:00:07 -0300

    Getting file, post created for adding points of interest

---
+++ CityDataAPI/Controllers/FilesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CityDataAPI.Controllers
+{
+    [Route("api/files")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        public readonly FileExtensionContentTypeProvider _fileExtensionContentTypeProvider;
+        
+        public FilesController(FileExtensionContentTypeProvider fileExtensionContentTypeProvider)
+        {
+            _fileExtensionContentTypeProvider = fileExtensionContentTypeProvider
+                ?? throw new System.ArgumentException(
+                    nameof(fileExtensionContentTypeProvider));    
+        }
+
+        [HttpGet("{fileId}")]
+        public ActionResult GetFile(string fileId)
+        {
+            var pathToFile = "Draft.pdf";
+
+            if (!System.IO.File.Exists(pathToFile))
+            {
+                return NotFound();
+            }
+
+            if(!_fileExtensionContentTypeProvider.TryGetContentType(
+                pathToFile, out var contentType))
+            {
+                contentType = "application/octet-stream";
+            }
+
+            var bytes = System.IO.File.ReadAllBytes(pathToFile);
+
+            return File(bytes, contentType, Path.GetFileName(pathToFile)); 
+
+        }
+    }
+}
--- CityDataAPI/Controllers/PointsOfInterestController.cs
+++ CityDataAPI/Controllers/PointsOfInterestController.cs
@@ -21,7 +21,7 @@ namespace CityDataAPI.Controllers
             return Ok(city.PointsOfInterest);
         }
 
-        [HttpGet("{pointofinterestid}")]    
+        [HttpGet("{pointofinterestid}", Name = "GetPointOfInterest")]    
         public ActionResult<PointOfInterestDto> GetPointOfInterest(int cityId, int pointOfInterestId) {
             var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
 
@@ -38,6 +38,39 @@ namespace CityDataAPI.Controllers
             }
 
             return Ok(pointOfInterest);
+
+        }
+        [HttpPost]
+        public ActionResult<PointOfInterestDto> CreatePointOfInterest(
+            int cityId,
+            PointOfInterestForCreationDto pointOfInterest)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(
+                c => c.PointsOfInterest).Max(p => p.Id);
+
+            var finalPointOfInterest = new PointOfInterestDto()
+            {
+                Id = ++maxPointOfInterestId,
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description,
+            };
+
+            city.PointsOfInterest.Add(finalPointOfInterest);
+
+            return CreatedAtRoute("GetPointOfInterest",
+                new
+                {
+                    cityId = cityId,
+                    pointOfinterestId = finalPointOfInterest.Id
+                },
+                finalPointOfInterest);
 
         }
     }
